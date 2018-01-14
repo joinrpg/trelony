9330e547a189120aae9f4699b30c987264939cdb
Leonid Tsarev <leo@bastilia.ru>
2018-01-14 17:54:44 +0300

    initial

---
+++ Joinrpg.Trelony.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Joinrpg.Trelony.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Joinrpg.Trelony.DataModel/AddUri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Joinrpg.Trelony.DataModel
+{
+    public class AddUri
+    {
+        public int AddUriId { get; set; }
+        public string Uri { get; set; }
+        public int? AllrpgInfoId { get; set; }
+        public bool Resolved { get; set; }
+    }
+}
