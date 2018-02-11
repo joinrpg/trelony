f6601c0180bb726ade652708a84cb6e1eb69ff74
Leonid Tsarev <leo@bastilia.ru>
2018-02-11 13:19:05 +0300

    add test games to seed

--- Joinrpg.Trelony.DataAccess/DbInitializer.cs
+++ Joinrpg.Trelony.DataAccess/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Joinrpg.Trelony.DataModel;
 using Microsoft.EntityFrameworkCore;
@@ -47,6 +48,81 @@ namespace Joinrpg.Trelony.DataAccess
 
                 context.MacroRegions.AddRange(northWest, center);
 
+                context.SaveChanges();
+            }
+
+            if (!context.Games.Any())
+            {
+                var games = new[]
+                {
+                    new Game()
+                    {
+                        Dates = new HashSet<GameDate>()
+                        {
+                            new GameDate()
+                            {
+                                Actual = true,
+                                GameDurationDays = 4,
+                                GameStartDate = new DateTime(year: 2018, month: 07, day: 25)
+                            }
+                        },
+                        Email = "klepa@example.com",
+                        GameName = "Железный трон",
+                        SubRegionId = context.SubRegions.Single(sr => sr.SubRegionName == "Московская область")
+                            .SubRegionId,
+                        GameStatus = GameStatus.ProbablyHappen,
+                        PlayersCount = 1500,
+                        VkontakteLink = "vesteros2018",
+                        Organizers = "МГ «Наррентурм»",
+                        GameType = GameType.Forest,
+                    },
+                    new Game()
+                    {
+                        Dates = new HashSet<GameDate>()
+                        {
+                            new GameDate()
+                            {
+                                Actual = true,
+                                GameDurationDays = 4,
+                                GameStartDate = new DateTime(year: 2018, month: 08, day: 15)
+                            }
+                        },
+                        Email = "freexee@example.com",
+                        GameName = "Магеллан: Время для звезд",
+                        SubRegionId = context.SubRegions.Single(sr => sr.SubRegionName == "Ленинградская область")
+                            .SubRegionId,
+                        GameStatus = GameStatus.ProbablyHappen,
+                        PlayersCount = 300,
+                        VkontakteLink = "magellan2018",
+                        Organizers = "МГ «Питерский формат»",
+                        GameUrl = "http://magellan2018.ru",
+                        GameType = GameType.OnRentedPlace,
+                    },
+                    new Game()
+                    {
+                        Dates = new HashSet<GameDate>()
+                        {
+                            new GameDate()
+                            {
+                                Actual = true,
+                                GameDurationDays = 4,
+                                GameStartDate = new DateTime(year: 2016, month: 07, day: 6)
+                            }
+                        },
+                        Email = "leo@example.com",
+                        GameName = "Стимпанк: Век разума",
+                        SubRegionId = context.SubRegions.Single(sr => sr.SubRegionName == "Ленинградская область")
+                            .SubRegionId,
+                        GameStatus = GameStatus.DefinitelyPassed,
+                        PlayersCount = 400,
+                        VkontakteLink = "steampunk2016",
+                        Organizers = "МГ «Питерский формат»",
+                        GameUrl = "http://steam2016.ru",
+                        GameType = GameType.Forest,
+                    },
+                };
+
+                context.Games.AddRange(games);
                 context.SaveChanges();
             }
         }
