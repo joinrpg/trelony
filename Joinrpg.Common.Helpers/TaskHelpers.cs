03f39ac2c9c3ae4f5e4dcf27490ebfeddbe6d60c
Leonid Tsarev <leo@bastilia.ru>
2018-04-07 17:14:19 +0300

    add task helpers

---
+++ Joinrpg.Common.Helpers/TaskHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joinrpg.Common.Helpers
+{
+    public static class TaskHelpers
+    {
+        public static Func<T1, Task<TOut>> SyncTask<T1, TOut>(Func<T1, TOut> mapper) where TOut : class
+        {
+            return i1 => Task.FromResult(mapper(i1));
+        }
+    }
+}
