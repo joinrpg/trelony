b97d457adc385939137bc5fed7a1d02b8063cea4
Leonid Tsarev <leo@bastilia.ru>
2018-04-07 16:29:57 +0300

    dictionary helpers

---
+++ Joinrpg.Common.Helpers/DictionaryHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using JetBrains.Annotations;
+
+namespace Joinrpg.Common.Helpers
+{
+    public static class DictionaryHelpers
+    {
+        [PublicAPI]
+        public static TValue GetValueOrDefault<TKey, TValue>([NotNull] this IReadOnlyDictionary<TKey, TValue> data, TKey key)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+            return data.ContainsKey(key) ? data[key] : default(TValue);
+        }
+
+        [PublicAPI]
+        public static IReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(
+            [NotNull] this IDictionary<TKey, TValue> dictionary)
+            => new ReadOnlyDictionary<TKey, TValue>(dictionary);
+    }
+}
