dee8bdfb94fc0864341d31568a1d4ba2da14622c
Leonid Tsarev <leo@bastilia.ru>
2018-05-27 19:15:48 +0300

    Add dummy login/logout

---
+++ Joinrpg.Trelony.WebBackend/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Joinrpg.Trelony.WebBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Joinrpg.Trelony.WebBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public Task Login(LoginViewModel viewModel)
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpPost]
+        public Task Logout() => Task.CompletedTask;
+    }
+}
